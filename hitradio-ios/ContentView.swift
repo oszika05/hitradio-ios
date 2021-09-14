9f9757a94a3fcc74f98bde48034563265f612986
Zsolt Oszlányi <zsolt.oscar@gmail.com>
2021-09-14 17:25:45 +0200

    Initial Commit

---
+++ hitradio-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hitradio-ios
+//
+//  Created by Zsolt Oszlányi on 2021. 09. 14..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hitradio-ios/hitradio_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  hitradio_iosApp.swift
+//  hitradio-ios
+//
+//  Created by Zsolt Oszlányi on 2021. 09. 14..
+//
+
+import SwiftUI
+
+@main
+struct hitradio_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
